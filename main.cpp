57daa4fe8c85aaaab7e09df69c57008cf6109a40
JoeyCluett <joesunglassescool@gmail.com>
2016-12-20 22:24:17 -0600

    first real commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+
+Network enabled game powered by OpenGL
+
+Players are simple 3D shapes. server
+passes position info between all players
+
+Each client has a thread devoted to
+network communication. network msgs
+are yet to be developed.
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
